e9fccb7f3332edd387a627acd803dba9d6b3f0a3
Nika Shabashvili <nikashabashvili@Nikas-MacBook-Pro.local>
2024-01-23 06:00:58 +0400

    prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "@/app/libs/prismadb"
 
+
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
