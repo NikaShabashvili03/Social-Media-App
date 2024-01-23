4307e83bfcd2b77d7612a17206f07b4f21bce5e9
Nika Shabashvili <nikashabashvili@Nikas-MacBook-Pro.local>
2024-01-23 08:39:11 +0400

    sd

--- next.config.mjs
+++ next.config.mjs
@@ -3,30 +3,16 @@ const nextConfig = {
     async headers() {
       return [
         {
-          // Routes this applies to
-          source: "/pages/api/(.*)",
-          // Headers
+          // matching all API routes
+          source: "/pages/api/:path*",
           headers: [
-            // Allow for specific domains to have access or * for all
-            {
-              key: "Access-Control-Allow-Origin",
-              value: "*",
-              // DOES NOT WORK
-              // value: process.env.ALLOWED_ORIGIN,
-            },
-            // Allows for specific methods accepted
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET, POST, PUT, DELETE, OPTIONS",
-            },
-            // Allows for specific headers accepted (These are a few standard ones)
-            {
-              key: "Access-Control-Allow-Headers",
-              value: "Content-Type, Authorization",
-            },
-          ],
-        },
-      ];
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
     },
     images: {
         domains: [
